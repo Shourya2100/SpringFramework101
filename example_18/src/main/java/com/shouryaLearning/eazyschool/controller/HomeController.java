b2ef32a0f9e29e64884f0fb02f157bf322d0829e
Shourya2100 <66374881+Shourya2100@users.noreply.github.com>
2024-02-16 23:26:15 +0530

    Hello World App using SpringBoot and Spring MVC

---
+++ example_18/src/main/java/com/shouryaLearning/eazyschool/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.shouryaLearning.eazyschool.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String displayHomePage()
+    {
+        return "Home.html";
+
+    }
+}
