ab4141647f154c3d5cb015864cacc6acefbb3470
Shourya2100 <66374881+Shourya2100@users.noreply.github.com>
2024-04-05 16:19:06 +0530

    Hello World App using Thymeleaf & DevTools setup

--- example_19/src/main/java/com/shouryaLearning/eazyschool/controller/HomeController.java
+++ example_19/src/main/java/com/shouryaLearning/eazyschool/controller/HomeController.java
@@ -2,14 +2,16 @@ package com.shouryaLearning.eazyschool.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String displayHomePage()
+    @RequestMapping(value = {"home", "", "/"})
+    public String displayHomePage(Model model)
     {
+        model.addAttribute("username","Shourya");
         return "home.html";
 
     }
