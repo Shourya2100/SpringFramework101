44500d2757eb34bcb2ace82d47491cdee0626bde
Shourya2100 <66374881+Shourya2100@users.noreply.github.com>
2024-04-16 11:39:35 +0100

    Configuring PermitAll and DenyAll using Spring Security

---
+++ example_27/src/main/java/com.shouryaLearning.eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.shouryaLearning.eazyschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        // Permit All Requests inside the Web Application
+        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        // Deny All Requests inside the Web Application
+            /*http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());*/
+
+        return http.build();
+
+    }
+
+}
