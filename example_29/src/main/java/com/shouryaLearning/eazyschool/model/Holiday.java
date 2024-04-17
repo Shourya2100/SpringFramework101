d82c59f964c469c8f1d32ad68bca70f611161cfb
Shourya2100 <66374881+Shourya2100@users.noreply.github.com>
2024-04-17 13:32:27 +0100

    Securing Web Application using inMemory users & Custom Login Page

---
+++ example_29/src/main/java/com/shouryaLearning/eazyschool/model/Holiday.java
@@ -0,0 +1,15 @@
+package com.shouryaLearning.eazyschool.model;
+
+import lombok.Data;
+
+@Data
+public class Holiday {
+
+    private final String day;
+    private final String reason;
+    private final Type type;
+
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+}
---
+++ example_29/src/main/java/com/shouryaLearning/eazyschool/service/ContactService.java
@@ -0,0 +1,44 @@
+package com.shouryaLearning.eazyschool.service;
+
+import com.shouryaLearning.eazyschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
+@Slf4j
+@Service
+// @RequestScope
+// @SessionScope
+@ApplicationScope
+public class ContactService {
+
+    private int counter = 0;
+
+    public ContactService(){
+        System.out.println("Contact Service Bean initialized");
+    }
+
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = true;
+        //TODO - Need to persist the data into the DB table
+        log.info(contact.toString());
+        return isSaved;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+}
