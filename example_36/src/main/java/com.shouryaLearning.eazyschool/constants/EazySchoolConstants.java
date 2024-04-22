09916f0bc877a71076f70a23a6afd508d5534cf3
Shourya2100 <66374881+Shourya2100@users.noreply.github.com>
2024-04-22 10:23:18 +0100

    Spring JPA Auditing using CreatedBy,LastModifiedBy,CreatedDate,LastModifiedDate

---
+++ example_36/src/main/java/com.shouryaLearning.eazyschool/constants/EazySchoolConstants.java
@@ -0,0 +1,7 @@
+package com.shouryaLearning.eazyschool.constants;
+
+public interface EazySchoolConstants {
+
+    public static final String OPEN = "Open";
+    public static final String CLOSE = "Close";
+}
