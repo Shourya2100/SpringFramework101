ac062cda5364c629bc31ae6edd8bfd212298d770
Shourya2100 <66374881+Shourya2100@users.noreply.github.com>
2024-05-02 11:36:25 +0100

    Implementing Properties Configuration & Spring Profiles

---
+++ example_49/src/main/java/com/shouryaLearning/eazyschool/repository/EazyClassRepository.java
@@ -0,0 +1,10 @@
+package com.shouryaLearning.eazyschool.repository;
+
+import com.shouryaLearning.eazyschool.model.EazyClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EazyClassRepository extends JpaRepository<EazyClass, Integer> {
+
+}
---
+++ example_49/src/main/java/com/shouryaLearning/eazyschool/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.shouryaLearning.eazyschool.repository;
+
+import com.shouryaLearning.eazyschool.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+    Person readByEmail(String email);
+
+}
